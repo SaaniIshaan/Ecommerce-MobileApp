cadc43462d1089c9ca508b37afd0868f30c6988a
unknown <guptamonika3017@gmail.com>
2023-11-04 13:19:45 -0700

    first commit

--- src/main/java/ecommercetest/EcommerceTest.java
+++ src/main/java/ecommercetest/EcommerceTest.java
@@ -3,6 +3,9 @@ package ecommercetest;
 import java.net.MalformedURLException;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
@@ -10,6 +13,8 @@ import org.testng.annotations.Test;
 import constants.WaitConstants;
 import ecommercepages.HomePage;
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.touch.offset.PointOption;
 import listeners.CommonListeners;
 
 
@@ -55,5 +60,32 @@ public class EcommerceTest extends BaseTest{
 		homepage.rotateScreen();
 	}
 	
+	@Test 
+	  public void SelectCategoryTC_05() throws InterruptedException {
+		  
+		  driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		  
+		  WebElement category =  driver.findElement(By.id("com.solodroid.solomerce:id/nav_category"));
+		  Point categotysize = category.getLocation();
+		  
+			System.out.println(categotysize.x);
+			System.out.println(categotysize.y); 
+			
+			TouchAction action = new TouchAction<>(driver);
+			action.tap(PointOption.point(categotysize.x, categotysize.y)).perform();
+	}
 	
-}
+	 @Test 
+	  public void SwipeActionTC_06() throws InterruptedException {
+		  
+		  driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		  WebElement search = driver.findElement(By.id("com.solodroid.solomerce:id/search"));
+		  
+		  TouchAction  swipeAction = new TouchAction<>(driver);
+		  swipeAction.press(PointOption.point( 900, 1800)).waitAction().moveTo(PointOption.point(70, 1800)).release().perform();
+		  Thread.sleep(5000);
+		  swipeAction.press(PointOption.point(70, 1800)).waitAction().moveTo(PointOption.point( 900, 1800)).release().perform();
+		  
+	 }
+}	
+	
